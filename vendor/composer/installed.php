feba6c5ea389a905bcb7e3a5b25fadaf17a33a4c
Ankush Tyagi <ankush.tyagi@squareboat.com>
2024-12-13 20:48:12 +0530

    WIP

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'randomstring/random-string-generator',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '01902b7e95ead789a87eeee417c6a375b691c322',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -110,9 +110,9 @@
             'dev_requirement' => true,
         ),
         'randomstring/random-string-generator' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '01902b7e95ead789a87eeee417c6a375b691c322',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
